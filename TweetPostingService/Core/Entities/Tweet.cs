ed5ad969f6c3919608bd0dcfb0a214406b3c94e0
Zr0den <axellund7@gmail.com>
2024-10-03 17:25:41 +0700

    Lavet lidt paa Tweet

--- TweetPostingService/Core/Entities/Tweet.cs
+++ TweetPostingService/Core/Entities/Tweet.cs
@@ -9,6 +9,8 @@ namespace TweetPostingService.Core.Entities
     public class Tweet
     {
         public int Id { get; set; }
+        public string Status { get; set; }
+        public int UserId { get; set; }
         public string Text { get; set; }
     }
 }
---
+++ TweetPostingService/Core/Helpers/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetPostingService.Core.Entities;
+
+namespace TweetPostingService.Core.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseInMemoryDatabase("TestDb");
+        }
+
+        public DbSet<Tweet> Tweet { get; set; }
+    }
+}
